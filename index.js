dbc397ce69be9e0b05ca04e43db4bad4bbc62a9d
Alrik Zachert <az@kreatiefer.de>
2017-09-05 00:53:26 +0200

    Add support for bundle all release notes schema into one export.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Modification = require('./definitions/modification.json');
+const ModificationList = require('./definitions/modification-list.json');
+const Release = require('./definitions/release.json');
+const ReleaseNotes = require('./definitions/release-notes.json');
+
+module.exports = {
+  Modification,
+  ModificationList,
+  Release,
+  ReleaseNotes,
+};
